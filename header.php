a1b2d935f73d66807ac4a613726ba78854ec6d53
nrouge212 <noha.rouge@gmail.com>
2026-09-08 11:19:24 +0200

    Amelioration projet fil rouge

--- header.php
+++ header.php
@@ -1,10 +1,16 @@
-<nav id="menu">
-    <div class="element_menu">
-        <h3>Menu de navigation</h3>
-        <ul>
-            <li><a href="page1.html">Lien</a></li>
-            <li><a href="page2.html">Lien</a></li>
-            <li><a href="page3.html">Lien</a></li>
-        </ul>
+<nav class="navbar navbar-expand-lg navbar-light bg-light mb-4 rounded">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="index.php">Mes Recettes</a>
+        
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="index.php">Accueil</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="contact.php">Contact</a>
+                </li>
+            </ul>
+        </div>
     </div>
 </nav>
