c4e86065826fdb71266b423f23d03f96b42a2c32
nrouge212 <noha.rouge@gmail.com>
2026-09-08 11:00:57 +0200

    Organisez le code PHP en blocs

---
+++ footer.php
@@ -0,0 +1,3 @@
+<footer>
+    <p>Copyright Noha Rougé, tous droits réservés</p>
+</footer>
--- index.php
+++ index.php
@@ -5,23 +5,12 @@
     <title>Mon site de recettes</title>
 </head>
 <body>
-    <nav id="menu">
-        <div class="element_menu">
-            <h3>Menu de navigation</h3>
-            <ul>
-                <li><a href="page1.html">Lien</a></li>
-                <li><a href="page2.html">Lien</a></li>
-                <li><a href="page3.html">Lien</a></li>
-            </ul>
-        </div>
-    </nav>
+    <?php include('header.php'); ?>
     <div>
         <h1>Mon site de recettes</h1>
         <p>Bienvenue sur mon site de recettes !</p>
     </div>
-    <footer>
-        <p>c'est un footer</p>
-    </footer>
+    <?php include('footer.php'); ?>
 
 </body>
 </html>
