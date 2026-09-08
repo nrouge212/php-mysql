1629dc05255ae112f843228c0a8ae2052d2862a1
nrouge212 <noha.rouge@gmail.com>
2026-09-08 11:12:55 +0200

    Projet fil rouge

---
+++ contact.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Site de recettes - Contact</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body class="d-flex flex-column min-vh-100">
+    <div class="container">
+        <?php include_once('header.php'); ?>
+        
+        <h1>Contactez nous</h1>
+        
+        <form action="contact.php" method="POST">
+            <div class="mb-3">
+                <label for="email" class="form-label">Email</label>
+                <input type="email" class="form-control" id="email" name="email">
+                <div id="email-help" class="form-text">Nous ne revendrons pas votre email.</div>
+            </div>
+            <div class="mb-3">
+                <label for="message" class="form-label">Votre message</label>
+                <textarea class="form-control" placeholder="Exprimez vous" id="message" name="message" style="height: 100px"></textarea>
+            </div>
+            <button type="submit" class="btn btn-primary">Envoyer</button>
+        </form>
+
+    </div>
+    <?php include_once('footer.php'); ?>
+</body>
+</html>
--- footer.php
+++ footer.php
@@ -1,3 +1,3 @@
-<footer>
+<footer class="text-center mt-5 mb-3">
     <p>Copyright Noha Rougé, tous droits réservés</p>
 </footer>
