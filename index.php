466b9d07e454c3f209c7a30d0f50124dc04a053a
nrouge212 <noha.rouge@gmail.com>
2026-09-08 10:54:25 +0200

    Découpage d'une page web

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Mon site de recettes</title>
+</head>
+<body>
+    <nav id="menu">
+        <div class="element_menu">
+            <h3>Menu de navigation</h3>
+            <ul>
+                <li><a href="page1.html">Lien</a></li>
+                <li><a href="page2.html">Lien</a></li>
+                <li><a href="page3.html">Lien</a></li>
+            </ul>
+        </div>
+    </nav>
+    <div>
+        <h1>Mon site de recettes</h1>
+        <p>Bienvenue sur mon site de recettes !</p>
+    </div>
+    <footer>
+        <p>c'est un footer</p>
+    </footer>
+
+</body>
+</html>
