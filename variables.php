3e804b31859085a41b5aee77dc8fce6543082ab4
nrouge212 <noha.rouge@gmail.com>
2026-09-15 10:27:57 +0200

    Ajout d'une clé password dans le tableau  de variables.php

--- variables.php
+++ variables.php
@@ -1,19 +1,23 @@
 <?php
+
 $users = [
     [
         'full_name' => 'John Doe',
         'email' => 'john.doe@gmail.com',
-        'age' => 35
+        'age' => 35,
+        'password' => 'john123' 
     ],
     [
         'full_name' => 'Jane Doe',
         'email' => 'jane.doe@gmail.com',
-        'age' => 54
+        'age' => 54,
+        'password' => 'jane123' 
     ],
     [
         'full_name' => 'Mickaël Andrieu',
         'email' => 'mickael.andrieu@exemple.com',
-        'age' => 34
+        'age' => 34,
+        'password' => 'mickael123' 
     ],
 ];
 
